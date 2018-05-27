27a870840ec82ab40f1a484d9a523b10a3e4847f
Kenzo Nojima <nojiken@nojimac.local>
2018-05-27 17:29:51 +0900

    update

---
+++ src/components/SearchPage.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import queryString from 'query-string';
+
+import SearchForm from '../containers/SearchForm';
+import GeocodeResult from './GeocodeResult';
+import Map from './Map';
+import HotelsTable from './HotelsTable';
+
+class SearchPage extends Component {
+
+  getPlaceParam() {
+    const params = queryString.parse(this.props.location.search);
+    const { place } = params;
+    if (place && place.length > 0) {
+      return place;
+    }
+    return null;
+  }
+
+  // handlePlaceSubmit(e) {
+  //   e.preventDefault();
+  //   this.props.history.push(`/?place=${this.state.place}`);
+  //   this.startSearch(this.state.place);
+  // }
+
+  render() {
+    return (
+      <div className="search-page">
+        <h1 className="app-title">ホテル検索</h1>
+        <SearchForm />
+        <div className="result-area">
+          <Map location={this.props.geocodeResult.location} />
+          <div className="result-right">
+            <GeocodeResult
+              address={this.props.geocodeResult.address}
+              location={this.props.geocodeResult.location}
+            />
+            <h2>ホテル検索結果</h2>
+            <HotelsTable />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+SearchPage.propTypes = {
+  // history: PropTypes.shape({ push: PropTypes.func }).isRequired,
+  location: PropTypes.shape({ search: PropTypes.string }).isRequired,
+  geocodeResult: PropTypes.shape({
+    address: PropTypes.string.isRequired,
+    location: PropTypes.shape({
+      lat: PropTypes.number.isRequired,
+      lng: PropTypes.number.isRequired,
+    }),
+  }).isRequired,
+};
+
+const mapStateToProps = state => ({
+  geocodeResult: state.geocodeResult,
+});
+
+export default connect(mapStateToProps)(SearchPage);
